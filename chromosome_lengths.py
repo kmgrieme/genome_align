53eabb48afed4492c39fc82434e511dfe6ca0bf7
katgor <kat.gorski@gmail.com>
2015-10-25 23:16:46 -0400

    quicy & dirty chr len simulated data
    needed for sam to maf as it stands only valid for primate

---
+++ chromosome_lengths.py
@@ -0,0 +1,17 @@
+sim_chr_len = dict()
+
+sim_chr_len["simChimp.chrA"] = 53121445
+sim_chr_len["simChimp.chrB"] = 85778862
+sim_chr_len["simChimp.chrC"] = 35661804
+sim_chr_len["simChimp.chrD"] = 10574168
+
+sim_chr_len["simGorilla.chrA"] = 53120926
+sim_chr_len["simGorilla.chrB"] = 85848133
+sim_chr_len["simGorilla.chrC"] = 35654756
+sim_chr_len["simGorilla.chrD"] = 10570608
+
+sim_chr_len["simOrang.chrB"] = 85903762
+sim_chr_len["simOrang.chrC"] = 35683973
+sim_chr_len["simOrang.chrD"] = 10564720
+sim_chr_len["simOrang.chrE"] = 37692687
+sim_chr_len["simOrang.chrF"] = 15493520
