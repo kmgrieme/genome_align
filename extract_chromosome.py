3aa11345c263d8631833120c24dd0a9d7ab02a6f
katgorski <kat.gorski@gmail.com>
2015-11-28 17:50:21 -0500

    new scripts to extract info from True mafs

---
+++ extract_chromosome.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import sys
+
+script, genome_file = sys.argv
+
+genome = open(genome_file)
+
+for line in genome:
+    if line[0] == ">":
+        file_name = line[1:] + ".fa"
+        working_file = open(file_name, "w")
+    working_file.write(line)
+
+working_file.close()
+genome.close()
---
+++ extract_reads_maf.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+import sys
+import re
+
+script, chromosome, in_maf, out_fq = sys.argv
+
+maf = open(in_maf)
+fq = open(out_fq, "w")
+
+threshold = 200
+step = 5
+
+for line in maf:
+    if line[0] == "a" or line[0] == "#": continue
+    if chromosome in line:
+        maf_string = re.split(r' +', line.strip())
+        chr_name = maf_string[1]
+        chr_index = int(maf_string[2])
+        fastq_header = '@%s_%s_L%s\n'
+        seq = re.sub(r'-+', '', maf_string[6])
+        seq = seq.upper()
+        if len(seq) > threshold:
+            for i in range(0, len(seq)-threshold, step):
+                working_index = chr_index + i
+                fq.write(fastq_header % (chr_name, working_index, threshold))
+                fq.write(seq[i:i+threshold])
+                fq.write("\n+\n")
+                fq.write("~"*threshold)
+                fq.write("\n")
+            last_seq = seq[i+threshold:]
+            fq.write(fastq_header % (chr_name, working_index+step, len(seq)-(i+threshold)))
+            fq.write(last_seq)
+            fq.write("\n+\n")
+            fq.write("~"*(len(last_seq)))
+            fq.write("\n")
+        else:    
+            fq.write(fastq_header % (chr_name, chr_index, len(seq)))
+            fq.write(seq)
+            fq.write("\n+\n")
+            fq.write("~"*len(seq))
+            fq.write("\n")
