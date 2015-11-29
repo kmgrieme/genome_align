84f93cc3184b7f5ed3788df050493b9bd29e8300
mxkatgor <kat.gorski@gmail.com>
2015-11-29 00:05:40 -0500

    maf to fastq updates

--- extract_reads_maf.py
+++ extract_reads_maf.py
@@ -19,23 +19,26 @@ for line in maf:
         fastq_header = '@%s_%s_L%s\n'
         seq = re.sub(r'-+', '', maf_string[6])
         seq = seq.upper()
-        if len(seq) > threshold:
-            for i in range(0, len(seq)-threshold, step):
-                working_index = chr_index + i
-                fq.write(fastq_header % (chr_name, working_index, threshold))
-                fq.write(seq[i:i+threshold])
-                fq.write("\n+\n")
-                fq.write("~"*threshold)
-                fq.write("\n")
-            last_seq = seq[i+threshold:]
-            fq.write(fastq_header % (chr_name, working_index+step, len(seq)-(i+threshold)))
-            fq.write(last_seq)
-            fq.write("\n+\n")
-            fq.write("~"*(len(last_seq)))
+        #if len(seq) > threshold:
+        #    for i in range(0, len(seq)-threshold, step):
+        #        working_index = chr_index + i
+        #        fq.write(fastq_header % (chr_name, working_index, threshold))
+        #        fq.write(seq[i:i+threshold])
+        #        fq.write("\n+\n")
+        #        fq.write("~"*threshold)
+        #        fq.write("\n")
+        #    last_seq = seq[i+threshold:]
+        #    fq.write(fastq_header % (chr_name, working_index+step, len(seq)-(i+threshold)))
+        #    fq.write(last_seq)
+        #    fq.write("\n+\n")
+        #    fq.write("~"*(len(last_seq)))
+        #    fq.write("\n")
+        #else:    
+        fq.write(fastq_header % (chr_name, chr_index, len(seq)))
+        fq.write(seq)
+        fq.write("\n+\n")
+        fq.write("~"*len(seq))
             fq.write("\n")
-        else:    
-            fq.write(fastq_header % (chr_name, chr_index, len(seq)))
-            fq.write(seq)
-            fq.write("\n+\n")
-            fq.write("~"*len(seq))
-            fq.write("\n")
+
+maf.close()
+fq.close()
